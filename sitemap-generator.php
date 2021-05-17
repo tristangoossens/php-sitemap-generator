27556baf720e669409fd1da196f0640c4634296c
Olivier <bourreauolivier@gmail.com>
2021-05-17 17:24:31 +0200

    Revert "Update sitemap-generator.php"

--- sitemap-generator.php
+++ sitemap-generator.php
@@ -156,7 +156,7 @@ class SitemapGenerator
 		}
 
 		$xml .= "</urlset>";
-		$xml = str_replace('&', '&amp;', $xml);
+
 		// Format string to XML
 		$dom = new DOMDocument;
 		$dom->preserveWhiteSpace = FALSE;
